c85c807283991b6f8504233d90a18307a6f6bb41
Martin Hilton <martin.hilton@canonical.com>
2018-07-06 07:18:17 +0100

    aclclient: add wrapper functions for client operations.

--- aclclient/client.go
+++ aclclient/client.go
@@ -1,6 +1,11 @@
 package aclclient
 
-import "gopkg.in/httprequest.v1"
+import (
+	"github.com/juju/aclstore/params"
+	"golang.org/x/net/context"
+	errgo "gopkg.in/errgo.v1"
+	"gopkg.in/httprequest.v1"
+)
 
 //go:generate httprequest-generate-client github.com/juju/aclstore handler client
 
@@ -23,4 +28,57 @@ func New(p NewParams) *Client {
 	c.Client.BaseURL = p.BaseURL
 	c.Client.Doer = p.Doer
 	return &c
+}
+
+// Get retrieves the contents of the given ACL.
+func (c *Client) Get(ctx context.Context, name string) ([]string, error) {
+	resp, err := c.GetACL(ctx, &params.GetACLRequest{
+		Name: name,
+	})
+	if err != nil {
+		return nil, errgo.Mask(err, isRemoteError)
+	}
+	return resp.Users, nil
+}
+
+// Set updates the contents of the given ACL to the given user list.
+func (c *Client) Set(ctx context.Context, name string, users []string) error {
+	err := c.SetACL(ctx, &params.SetACLRequest{
+		Name: name,
+		Body: params.SetACLRequestBody{
+			Users: users,
+		},
+	})
+	return errgo.Mask(err, isRemoteError)
+}
+
+// Add updates the contents of the given ACL to include the given user
+// list.
+func (c *Client) Add(ctx context.Context, name string, users []string) error {
+	err := c.ModifyACL(ctx, &params.ModifyACLRequest{
+		Name: name,
+		Body: params.ModifyACLRequestBody{
+			Add: users,
+		},
+	})
+	return errgo.Mask(err, isRemoteError)
+}
+
+// Remove updates the contents of the given ACL to remove those in the
+// given user list.
+func (c *Client) Remove(ctx context.Context, name string, users []string) error {
+	err := c.ModifyACL(ctx, &params.ModifyACLRequest{
+		Name: name,
+		Body: params.ModifyACLRequestBody{
+			Remove: users,
+		},
+	})
+	return errgo.Mask(err, isRemoteError)
+}
+
+// isRemoteError determines whether the given error is a
+// httprequest.RemoteError.
+func isRemoteError(err error) bool {
+	_, ok := err.(*httprequest.RemoteError)
+	return ok
 }
